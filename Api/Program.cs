4b519b2ac1e760168dccb544f209755e64df37cd
Kainan H. <kainanhenriq18@hotmail.com>
2026-04-21 18:18:24 -0300

    Adicionado comentários em Program.cs

--- Api/Program.cs
+++ Api/Program.cs
@@ -6,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Banco de dados SQLite (cria o arquivo "data.db" automaticamente)
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite("Data Source=data.db"));
+    options.UseSqlite("Data Source=data.db")
+);
 
 // Registra os repositórios para injeção de dependência
 builder.Services.AddScoped<EventoRepository>();
@@ -19,13 +20,17 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Cria as tabelas no banco automaticamente na primeira execução
-using (var scope = app.Services.CreateScope())
-{
+using (var scope = app.Services.CreateScope()) {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.EnsureCreated();
 }
 
+// Instancia Swegger para documentação da API
 app.UseSwagger();
 app.UseSwaggerUI();
+
+// Mapeia os controladores para as rotas da API
 app.MapControllers();
+
+// Inicia a aplicação
 app.Run();
