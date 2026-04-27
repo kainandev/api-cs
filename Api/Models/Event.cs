c821fd3bc6bb94753ba855d000ea1b87164636bd
Lívia Tarasiuk <163937453+viasiuk@users.noreply.github.com>
2026-04-27 16:38:11 -0300

    Update Event.cs
    Com descrição

--- Api/Models/Event.cs
+++ Api/Models/Event.cs
@@ -3,17 +3,17 @@ using Api.Models;
 namespace Api.Models {
     // Representa um evento criado por um organizador na plataforma
     public class Event {
-        public int Id { get; set; }
-        public string Name { get; set; } = "";
-        public string Address { get; set; } = "";
-        public string Description { get; set; } = "";
-        public DateTime Date { get; set; }
-        public int MinAge { get; set; } = 0;
-        public EventStatus Status { get; set; } = EventStatus.Active;
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public int Id { get; set; }   //ID único
+        public string Name { get; set; } = "";  //Nome do evento, por exemplo, "Show do Coldplay"
+        public string Address { get; set; } = "";  //endereço/local
+        public string Description { get; set; } = "";  //sobre o evento
+        public DateTime Date { get; set; }  //data e horário
+        public int MinAge { get; set; } = 0;  //restirngir para menores de x anos.
+        public EventStatus Status { get; set; } = EventStatus.Active;  //Se o evento está ativo, para que um evento com data no passado não ocorra no futuro
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;  //Data de criação/registro
 
         // Chave estrangeira: usuário que criou o evento
-        public string OwnerId { get; set; } = "";
+        public string OwnerId { get; set; } = ""; 
 
         // Lotes de ingressos criados para este evento (relação 1:N)
         public List<EventTicket> EventTickets { get; set; } = new();
