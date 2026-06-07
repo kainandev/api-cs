eb808c0b9a6ea6f47e74ab208aa38abb13c051a4
Kainan H. <kainanhenriq18@hotmail.com>
2026-06-07 01:55:35 -0300

    Adicionado import para correção de renderização da página de 'Meus Ingressos'

--- InterfaceTicket/src/pages/myTickets/MyTickets.jsx
+++ InterfaceTicket/src/pages/myTickets/MyTickets.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../assets/components/Header/header.jsx';
 import { api } from '../../services/api';
 import './MyTickets.css';
